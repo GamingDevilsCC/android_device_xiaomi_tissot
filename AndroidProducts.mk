d8376d0f15741a6ecc100caaf4b70ab8e3f61c78
GamingDevilsCC <gamingdevilscc@gmail.com>
2018-04-17 21:46:27 -0400

    Liquid-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 # 
-# Copyright (C) 2017 The LineageOS Open Source Project 
+# Copyright (C) 2017 The LineageOS Open Source Project
+# Copyright (C) 2017 The Liquid Remix Open Source Project
 # 
 # Licensed under the Apache License, Version 2.0 (the "License"); 
 # you may not use this file except in compliance with the License. 
@@ -14,5 +15,5 @@
 # limitations under the License. 
 # 
  
-PRODUCT_MAKEFILES := \ 
-    $(LOCAL_DIR)/lineage.mk 
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/liquid.mk 
